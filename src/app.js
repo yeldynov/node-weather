d1c73ad4dc0f3491d36e0f295edb958b940d3c2a
Nikita Yeldynov <yeldynov@gmail.com>
2022-11-07 19:38:23 +0200

    changed port for heroku

--- src/app.js
+++ src/app.js
@@ -90,6 +90,4 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log('Server is up on port ' + port);
-});
+app.listen(port, () => console.log('Server is up on port ' + port));
