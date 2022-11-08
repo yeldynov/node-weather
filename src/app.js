1ac0f1e1aa529f5ada95e4e63b136a489144f481
Nikita Yeldynov <yeldynov@gmail.com>
2022-11-08 09:44:53 +0200

    updated dependencies

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../views');
 const partialsPath = path.join(__dirname, '../views/partials');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath));
